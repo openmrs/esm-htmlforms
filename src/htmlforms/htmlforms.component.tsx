e24dd752fec7103d7301d39cc8d147ddbc239680
Brandon Istenes <bistenes@gmail.com>
2019-11-12 17:12:11 -0600

    Initialize module

---
+++ src/htmlforms/htmlforms.component.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./htmlforms.component.css";
+
+export default function HtmlForms() {
+  return (
+    <div className={`canvas ${styles["container"]}`}>Forms go here.</div>
+  )
+}
